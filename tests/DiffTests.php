189cbc8f29fdcb5caa8868574ac8e00b4726ac9b
egorskov <egorshkov93@gmail.com>
2024-12-07 22:09:04 +0500

    tests

--- tests/DiffTests.php
+++ tests/DiffTests.php
@@ -9,8 +9,8 @@ class DiffTests extends TestCase
 {
     public function testDiff()
     {
-        $expected = file_get_contents("resultJson.txt");
-        $this->assertEquals($expected, genDiff("file1.json", "/Users/egorgorskov/file2.json"));
+        $expected = file_get_contents(__DIR__."/fixtures/resultJson.txt");
+        $this->assertEquals($expected, genDiff(__DIR__."/fixtures/testfile1.json", __DIR__."/fixtures/testfile2.json"));
     }
 }
 
